4658494b69316bfe3dda11f56929e7142eff9a1a
Aditya Kumar Singh <adityasinghssj1@gmail.com>
2019-06-27 00:15:27 +0530

    JSX module completed

--- react-training-modules/jsx/src/index.js
+++ react-training-modules/jsx/src/index.js
@@ -1,7 +1,6 @@
 // import the react and react-dom libraries 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Ex1 from './Ex1'
 
 // create a react component
 const App = () => {
@@ -9,7 +8,6 @@ const App = () => {
 
     return (
         <div>
-            <Ex1 />
             <label className='label' htmlFor='name'> Enter Name: </label>
             <input id="name" type="text" />
             <button style={ { backgroundColor: 'blue', color : 'white'} }>{ buttonText.text }</button>
