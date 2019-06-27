d11c0268563314468c27b2fa6d64d41529e95465
Aditya Kumar Singh <adityasinghssj1@gmail.com>
2019-06-27 23:58:38 +0530

    Comment section structure added.

--- react-training-modules/components/src/index.js
+++ react-training-modules/components/src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <div className="comments">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Adi
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 11:30pm</span>
+          </div>
+          <div className="text">
+            Pretty complex code
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
