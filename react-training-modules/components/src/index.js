689b9ed66e5d04ba6d44f68b1cd7f771eb00202e
Aditya Kumar Singh <adityasinghssj1@gmail.com>
2019-07-08 19:48:22 +0530

    comment component fixed

--- react-training-modules/components/src/index.js
+++ react-training-modules/components/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comments">
+      <div className="comment">
         <a href="/" className="avatar">
           <img alt="avatar" />
         </a>
