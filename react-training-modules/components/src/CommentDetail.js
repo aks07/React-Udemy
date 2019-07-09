65cfedbe1cfbf0cdcd9c3271390a35eae52ff7a0
Aditya Kumar Singh <adityasinghssj1@gmail.com>
2019-07-09 23:54:12 +0530

    Added basic approval card

--- react-training-modules/components/src/CommentDetail.js
+++ react-training-modules/components/src/CommentDetail.js
@@ -1,18 +1,17 @@
 import React from 'react'
-import faker from 'faker'
 
-const CommentDetail = ({author, time, comment}) => {
+const CommentDetail = ({author, time, comment, avatar}) => {
     return (
         <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()}/>
+          <img alt="avatar" src={avatar}/>
         </a>
         <div className="content">
           <a href="/" className="author">
             {author}
           </a>
           <div className="metadata">
-            <span className="date">Today at {time}</span>
+            <span className="date">{time}</span>
           </div>
           <div className="text">
             {comment}
--- react-training-modules/components/src/index.js
+++ react-training-modules/components/src/index.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import faker from 'faker'
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard />
       <CommentDetail
         author="Adi" 
-        time="11:30pm"
-        comment="Hello from Adi"
+        time="Today at 11:30pm"
+        avatar= {faker.image.avatar()}
+        comment="Nice Blog"
       />
       <CommentDetail
         author="Nishu" 
-        time="08:00pm"
-        comment="Hello from Nishu"
+        time="Yesterday at 08:00pm"
+        avatar= {faker.image.avatar()}
+        comment="Very Informative"
       />
       <CommentDetail
         author="Parul" 
-        time="08:30pm"
-        comment="Hello from Parul"
+        time="Today at 08:30pm"
+        avatar= {faker.image.avatar()}
+        comment="You're great"
       />
     </div>
   )
