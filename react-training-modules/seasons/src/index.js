29ffc7d493f1fac270db538a2c64bd7df9d65c6c
Aditya Kumar Singh <adityasinghssj1@gmail.com>
2019-09-18 22:51:07 +0530

    initialized seasons app. recieved user location

--- react-training-modules/seasons/src/index.js
+++ react-training-modules/seasons/src/index.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 class App extends React.Component{
+
+    constructor(props) {
+        super(props);
+        // This is the only time we use this.state, dont ever use it to update state
+        this.state = {
+            lat: null,
+            errorMessage: ''
+        }
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat: position.coords.latitude})
+            },
+            (error) => {
+                this.setState({errorMessage: error.message})
+            }
+        )
+    }
+
     render() {
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if(!this.state.errorMessage && this.state.lat){
+            return <div>Latitude: {this.state.lat}</div>
+        }
         return (
-            <div>
-                Hello from index for seasons
-            </div>
+            <div>Loading----</div>
         )
     }
 }
